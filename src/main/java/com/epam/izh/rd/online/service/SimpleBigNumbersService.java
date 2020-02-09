d76d1c13e60fa5c3f42560a8ceb86b6794d6f00e
Sergey Pertsev <s.pertsev@outlook.com>
2020-02-10 01:12:44 +0300

    The methods implemented in classes 'SimpleBigNumbersService.java' and 'SimpleDateService.java'.

--- src/main/java/com/epam/izh/rd/online/service/SimpleBigNumbersService.java
+++ src/main/java/com/epam/izh/rd/online/service/SimpleBigNumbersService.java
@@ -2,18 +2,22 @@ package com.epam.izh.rd.online.service;
 
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.RoundingMode;
 
 public class SimpleBigNumbersService implements BigNumbersService {
 
     /**
      * Метод делит первое число на второе с заданной точностью
      * Например 1/3 с точностью 2 = 0.33
+     *
      * @param range точность
      * @return результат
      */
     @Override
     public BigDecimal getPrecisionNumber(int a, int b, int range) {
-        return null;
+        BigDecimal decimalA = BigDecimal.valueOf(a);
+        BigDecimal decimalB = BigDecimal.valueOf(b);
+        return decimalA.divide(decimalB, range, RoundingMode.HALF_UP);
     }
 
     /**
@@ -24,6 +28,14 @@ public class SimpleBigNumbersService implements BigNumbersService {
      */
     @Override
     public BigInteger getPrimaryNumber(int range) {
-        return null;
+        BigInteger currentNumber = new BigInteger("2");
+        int counter = 0;
+        while (counter < range) {
+            currentNumber = currentNumber.add(BigInteger.ONE);
+            if (currentNumber.isProbablePrime(5)) {
+                counter++;
+            }
+        }
+        return currentNumber;
     }
 }
